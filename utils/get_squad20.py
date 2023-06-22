186a14f5eff121ee4a24992d5120727d93a2c66e
Ed Brown <teddybrown@ischool.berkeley.edu>
2023-06-22 11:38:00 -0700

    exploring huggingface

--- utils/get_squad20.py
+++ utils/get_squad20.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from transformers import TFBertForQuestionAnswering, BertTokenizer
+from transformers import TFBertForQuestionAnswering, TFBertTokenizer
 
 (ds_train, ds_validation), ds_info = tfds.load(
     "squad/v2.0", split=["train", "validation"], shuffle_files=True, with_info=True
@@ -11,26 +11,9 @@ from transformers import TFBertForQuestionAnswering, BertTokenizer
 bert_model = TFBertForQuestionAnswering.from_pretrained(
     "bert-large-uncased-whole-word-masking-finetuned-squad"
 )
-bert_tokenizer = BertTokenizer.from_pretrained(
+bert_tokenizer = TFBertTokenizer.from_pretrained(
     "bert-large-uncased-whole-word-masking-finetuned-squad"
 )
 
 ds_train_np = tfds.as_numpy(ds_train)
-
-bert_train_tokenized = bert_tokenizer(
-        ([t['question'] for t in ds_train_np],
-    [t['context'] for t in ds_train_np] ),
-    padding=True,
-    truncation=True,
-    max_length=384,
-    return_tensors="tf",
-)
-
-bert_train_inputs = {
-    k: bert_train_tokenized[k]
-    for k in ["input_ids", "token_type_ids", "attention_mask"]
-}
-bert_train_labels = {
-    "start_positions": bert_train_tokenized["start_positions"],
-    "end_positions": bert_train_tokenized["end_positions"],
-}
+ds_validation_np = tfds.as_numpy(ds_validation)
