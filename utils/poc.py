8909b32d27b5f62f5cbacaa2d6aaa2bb57a45fd3
Ed Brown <76535893+teddy661@users.noreply.github.com>
2023-06-25 08:58:58 -0700

    change to huggingface dataset from tdfs

--- utils/poc.py
+++ utils/poc.py
@@ -1,27 +1,30 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-import tensorflow_datasets as tfds
+from datasets import load_dataset
 from transformers import (
     BertConfig,
     TFBertForQuestionAnswering,
     BertTokenizer,
 )
+from pathlib import Path
 
-(ds_train, ds_validation), ds_info = tfds.load(
-    "squad/v2.0", split=["train", "validation"], shuffle_files=True, with_info=True
-)
+# setup for multi-gpu training
+mirrored_strategy = tf.distribute.MirroredStrategy()
+checkpoint_dir = Path(r"./training_checkpoints")
+checkpoint_fullpath = checkpoint_dir.joinpath("ckpt_{epoch}")
 
+# (ds_train, ds_validation), ds_info = tfds.load(
+#    "squad/v2.0", split=["train", "validation"], shuffle_files=True, with_info=True
+# )
+ds = load_dataset("squad_v2")
 
 bert_tokenizer = BertTokenizer.from_pretrained(
     "bert-large-uncased-whole-word-masking-finetuned-squad"
 )
 
-ds_train_input = [x for x in tfds.as_numpy(ds_train)]
-ds_validation_input = [x for x in tfds.as_numpy(ds_validation)]
-
-train_question = [x["question"].decode("utf-8") for x in ds_train_input[:10]]
-train_context = [x["context"].decode("utf-8") for x in ds_train_input[:10]]
+train_question = ds["train"]["question"][:100]
+train_context = ds["train"]["context"][:100]
 
 max_seq_length = 512
 
@@ -36,53 +39,62 @@ train_encodings = bert_tokenizer(
 )
 
 
-def create_bert_qa_model():
-    bert_config = BertConfig.from_pretrained(
-        "bert-large-uncased-whole-word-masking-finetuned-squad",
-        output_hidden_states=True,
-    )
+def create_bert_qa_model(
+    MODEL_NAME="bert-large-uncased-whole-word-masking-finetuned-squad",
+):
+    with mirrored_strategy.scope():
+        bert_config = BertConfig.from_pretrained(
+            MODEL_NAME,
+            output_hidden_states=True,
+        )
 
-    bert_model = TFBertForQuestionAnswering.from_pretrained(
-        "bert-large-uncased-whole-word-masking-finetuned-squad", config=bert_config
-    )
+        bert_model = TFBertForQuestionAnswering.from_pretrained(
+            MODEL_NAME, config=bert_config
+        )
 
-    input_ids = tf.keras.layers.Input(
-        shape=(max_seq_length,), dtype=tf.int64, name="input_ids"
-    )
-    attention_mask = tf.keras.layers.Input(
-        shape=(max_seq_length,), dtype=tf.int64, name="input_masks"
-    )
-    token_type_ids = tf.keras.layers.Input(
-        shape=(max_seq_length,), dtype=tf.int64, name="token_type_ids"
-    )
+        input_ids = tf.keras.layers.Input(
+            shape=(max_seq_length,), dtype=tf.int64, name="input_ids"
+        )
+        attention_mask = tf.keras.layers.Input(
+            shape=(max_seq_length,), dtype=tf.int64, name="input_masks"
+        )
+        token_type_ids = tf.keras.layers.Input(
+            shape=(max_seq_length,), dtype=tf.int64, name="token_type_ids"
+        )
 
-    bert_inputs = {
-        "input_ids": input_ids,
-        "token_type_ids": token_type_ids,
-        "attention_mask": attention_mask,
-    }
+        bert_inputs = {
+            "input_ids": input_ids,
+            "token_type_ids": token_type_ids,
+            "attention_mask": attention_mask,
+        }
 
-    bert_output = bert_model(bert_inputs)
+        bert_output = bert_model(bert_inputs)
 
-    start_logits = bert_output.start_logits
-    end_logits = bert_output.end_logits
+        start_logits = bert_output.start_logits
+        end_logits = bert_output.end_logits
 
-    softmax_start_logits = tf.keras.layers.Softmax()(start_logits)
-    softmax_end_logits = tf.keras.layers.Softmax()(end_logits)
+        softmax_start_logits = tf.keras.layers.Softmax()(start_logits)
+        softmax_end_logits = tf.keras.layers.Softmax()(end_logits)
 
-    # Need to do argmax after softmax to get most likely index
-    bert_qa_model = tf.keras.Model(
-        inputs=[input_ids, token_type_ids, attention_mask],
-        outputs=[softmax_start_logits, softmax_end_logits],
-    )
+        # Need to do argmax after softmax to get most likely index
+        bert_qa_model = tf.keras.Model(
+            inputs=[input_ids, token_type_ids, attention_mask],
+            outputs=[softmax_start_logits, softmax_end_logits],
+        )
 
-    bert_qa_model.compile(
-        optimizer=tf.keras.optimizers.Adam(
-            learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0
-        ),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-        metrics=[tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy")],
-    )
+        bert_qa_model.compile(
+            optimizer=tf.keras.optimizers.Adam(
+                learning_rate=3e-5, epsilon=1e-08, clipnorm=1.0
+            ),
+            loss=[
+                tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+            ],
+            metrics=[
+                tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+                tf.keras.metrics.SparseCategoricalAccuracy(name="accuracy"),
+            ],
+        )
     return bert_qa_model
 
 
@@ -106,6 +118,13 @@ def combine_bert_subwords(bert_tokenizer, encodings, predictions):
 bert_qa_model = create_bert_qa_model()
 # tf.keras.utils.plot_model(bert_qa_model, show_shapes=True)
 bert_qa_model.summary()
+callbacks = [
+    tf.keras.callbacks.ModelCheckpoint(
+        filepath=checkpoint_fullpath, save_weights_only=True
+    ),
+]
+
+print("Before training model...")
 
 predictions = bert_qa_model.predict(
     [
@@ -116,13 +135,12 @@ predictions = bert_qa_model.predict(
 )
 answers = combine_bert_subwords(bert_tokenizer, train_encodings, predictions)
 
-for i, q in enumerate(train_question):
+for i, q in enumerate(train_question[:100]):
     print(f"Question: {q}")
     print(f"Predicted Answer: {answers[i]}")
-    if ds_train_input[i]["is_impossible"]:
-        print(
-            f"Plausible Answer: {ds_train_input[i]['plausible_answers']['text'][0].decode('utf-8')}"
-        )
-    else:
-        print(f"Answer: {ds_train_input[i]['answers']['text'][0].decode('utf-8')}")
+    print(f"Answer: {ds['train']['answers'][i]['text'][0]}")
     print(80 * "=")
+
+print("Training model...")
+
+# bert_qa_model.fit(
