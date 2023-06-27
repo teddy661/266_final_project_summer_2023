98c8f566df2d98c065a161a3440d2ca37702ef05
Ed Brown <76535893+teddy661@users.noreply.github.com>
2023-06-27 06:35:22 -0700

    understand how map works for tensorflow_datasets

--- data/tensorflowds.py
+++ data/tensorflowds.py
@@ -1,6 +1,15 @@
+import tensorflow as tf
 import tensorflow_datasets as tfds
 
-(squadv2_train, squadv2_validation), squadv2_info = tfds.load("squad/v2.0", split=["train", "validation"], shuffle_files=False, with_info=True)
+(squadv2_train, squadv2_validation), squadv2_info = tfds.load(
+    "squad/v2.0", split=["train", "validation"], shuffle_files=False, with_info=True
+)
 
-squadv2_train_input = [x for x in tfds.as_numpy(squadv2_train)]
-squadv2_validation_input = [x for x in tfds.as_numpy(squadv2_validation)]
+input = squadv2_train.map(
+    lambda x: {
+        "question": x["question"],
+        "context": x["context"],
+        "answers": x["answers"],
+    },
+    num_parallel_calls=tf.data.AUTOTUNE,
+)
