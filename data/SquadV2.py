be3371dba918c9981cc4243bcd1bde1e72ef647e
Ed Brown <76535893+teddy661@users.noreply.github.com>
2023-06-30 18:23:29 -0700

    change to a class

--- data/SquadV2.py
+++ data/SquadV2.py
@@ -1,18 +1,42 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-(squadv2_train, squadv2_validation), squadv2_info = tfds.load(
-    "squad/v2.0", split=["train", "validation"], shuffle_files=False, with_info=True
-)
 
-input = squadv2_train.map(
-    lambda x: {
-        "question": x["question"],
-        "context": x["context"],
-        "answers": x["answers"],
-    },
-    num_parallel_calls=tf.data.AUTOTUNE,
-)
+class SquadV2:
 
-input = input.cache()
-input = input.prefetch(tf.data.AUTOTUNE)
+    def __init__(self):
+        self.train = None
+        self.validation = None
+        self.info = None
+
+    def load_data(self):
+        (ds_train, ds_validation), self.info = tfds.load(
+            "squad/v2.0", split=["train", "validation"], shuffle_files=False, with_info=True
+        )
+
+        ds_train = ds_train.cache()
+        ds_train = ds_train.shuffle(self.info.splits['train'].num_examples)
+        self.train = ds_train.prefetch(tf.data.AUTOTUNE)
+
+        ds_validation = ds_validation.cache()
+        ds_validation = ds_validation.shuffle(self.info.splits['train'].num_examples)
+        self.validation = ds_validation.prefetch(tf.data.AUTOTUNE)
+
+    def get_raw_train_data(self):
+        return self.train
+    
+    def get_raw_validation_data(self):
+        return self.validation
+    
+    def get_train_data(self, num_samples):
+        if num_samples is None:
+            num_samples=self.train.cardinality().numpy()
+        return [x for x in tfds.as_numpy(self.train.take())]
+    
+    def get_validation_data(self, num_samples):
+        if num_samples is None: 
+            num_samples=self.validation.cardinality().numpy()
+        return [x for x in tfds.as_numpy(self.validation.take())]
+    
+    def get_info(self):
+        return self.info
