f9df692e3d8c9fda0586b23e448cafeca6f34cc5
Ed Brown <76535893+teddy661@users.noreply.github.com>
2023-06-30 19:19:25 -0700

    fix num_samples

--- data/SquadV2.py
+++ data/SquadV2.py
@@ -31,12 +31,12 @@ class SquadV2:
     def get_train_data(self, num_samples):
         if num_samples is None:
             num_samples=self.train.cardinality().numpy()
-        return [x for x in tfds.as_numpy(self.train.take())]
+        return [x for x in tfds.as_numpy(self.train.take(num_samples))]
     
     def get_validation_data(self, num_samples):
         if num_samples is None: 
             num_samples=self.validation.cardinality().numpy()
-        return [x for x in tfds.as_numpy(self.validation.take())]
+        return [x for x in tfds.as_numpy(self.validation.take(num_samples))]
     
     def get_info(self):
         return self.info
