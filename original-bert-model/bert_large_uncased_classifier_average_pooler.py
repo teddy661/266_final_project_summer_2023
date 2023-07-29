bd1bfcec19e58555df1eccde8baeebb69fa1a2ec
Ed Brown <76535893+teddy661@users.noreply.github.com>
2023-07-29 08:43:18 -0700

    first pass at somewhat working average pooler, need to implement loading weights, then can train

---
+++ original-bert-model/bert_large_uncased_classifier_average_pooler.py
@@ -0,0 +1,56 @@
+import os
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import tensorflow as tf
+
+tf.get_logger().setLevel("INFO")
+from transformers import BertConfig, TFBertModel
+
+
+def create_bert_classifier_average_pooler(
+    MODEL_NAME="bert-large-uncased", optimizer=None, max_seq_length=512
+):
+    """
+    Creates a BERT QA model using the HuggingFace transformers library
+    and base bert-large-uncased model. T
+    """
+    bert_config = BertConfig.from_pretrained(
+        MODEL_NAME,
+        output_hidden_states=True,
+    )
+
+    bert_model = TFBertModel.from_pretrained(MODEL_NAME, config=bert_config)
+    bert_model.trainable = False
+
+    input_ids = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int64, name="input_ids"
+    )
+    token_type_ids = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int64, name="token_type_ids"
+    )
+    attention_mask = tf.keras.layers.Input(
+        shape=(max_seq_length,), dtype=tf.int64, name="input_masks"
+    )
+
+    bert_inputs = {
+        "input_ids": input_ids,
+        "token_type_ids": token_type_ids,
+        "attention_mask": attention_mask,
+    }
+
+    # Not using pooler_output
+    pooler_output = (bert_model(bert_inputs)).pooler_output
+    concat_hidden_states = tf.concat(bert_model(bert_inputs).hidden_states, axis=-1)
+    average_pooler_layer = tf.reduce_mean(concat_hidden_states, axis=-1, keepdims=False)
+    concat_pooler_output = tf.concat([pooler_output, average_pooler_layer], axis=-1)
+    dropout_layer = tf.keras.layers.Dropout(0.1)(concat_pooler_output)
+    classifier_layer = tf.keras.layers.Dense(
+        1, activation="sigmoid", name="classifier"
+    )(dropout_layer)
+
+    bert_classifier_model = tf.keras.Model(
+        inputs=[input_ids, token_type_ids, attention_mask],
+        outputs=[classifier_layer],
+        name=f"bert_large_classifier_average_pooler",
+    )
+    return bert_classifier_model
---
+++ original-bert-model/train_bert_large_uncased_classifier_average_pooler.py
@@ -0,0 +1,121 @@
+import json
+import os
+import pickle
+import sys
+from collections import Counter, defaultdict
+from pathlib import Path
+
+import joblib
+import numpy as np
+import pandas as pd
+from transformers import BertConfig, BertTokenizer, TFBertModel, WarmUp
+
+from bert_large_uncased_classifier_average_pooler import (
+    create_bert_classifier_average_pooler,
+)
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+import tensorflow as tf
+
+tf.get_logger().setLevel("INFO")
+
+
+if "__file__" in globals():
+    script_path = Path(__file__).parent.absolute()
+else:
+    script_path = Path.cwd()
+
+data_dir = script_path.joinpath("../data")
+validation_data_path = data_dir.joinpath("imdb_validation_data.pkl")
+# setup for multi-gpu training
+
+for percent_data in [20, 40, 60, 80, 100]:
+    tf.keras.backend.clear_session()
+    if "bert_classifier_average_pooler_model" in globals():
+        del bert_classifier_average_pooler_model
+    training_data_path = data_dir.joinpath(f"imdb_training_data_{percent_data}.pkl")
+    training_data = joblib.load(training_data_path)
+    validation_data = joblib.load(validation_data_path)
+    mirrored_strategy = tf.distribute.MirroredStrategy()
+
+    checkpoint_dir = script_path.joinpath(
+        f"training_checkpoints_classifier_average_pooler{percent_data}"
+    )
+    checkpoint_fullpath = checkpoint_dir.joinpath("ckpt_{epoch:04d}.ckpt")
+
+    train_input_ids = training_data["input_ids"]
+    train_token_type_ids = training_data["token_type_ids"]
+    train_attention_mask = training_data["attention_mask"]
+    train_labels = training_data["labels"]
+
+    val_input_ids = validation_data["input_ids"]
+    val_token_type_ids = validation_data["token_type_ids"]
+    val_attention_mask = validation_data["attention_mask"]
+    val_labels = validation_data["labels"]
+
+    if percent_data == 100:
+        epochs = 6
+    else:
+        epochs = 1
+    batch_size = 30
+    steps_per_epoch = len(train_input_ids) // batch_size
+    num_train_steps = steps_per_epoch * epochs
+    warmup_steps = num_train_steps // 10
+    initial_learning_rate = 2e-5
+
+    with mirrored_strategy.scope():
+        linear_decay = tf.keras.optimizers.schedules.PolynomialDecay(
+            initial_learning_rate=initial_learning_rate,
+            end_learning_rate=0,
+            decay_steps=num_train_steps,
+        )
+
+        warmup_schedule = WarmUp(
+            initial_learning_rate=0,
+            decay_schedule_fn=linear_decay,
+            warmup_steps=warmup_steps,
+        )
+
+        optimizer = tf.keras.optimizers.AdamW(learning_rate=warmup_schedule)
+
+        bert_classifier_average_pooler_model = create_bert_classifier_average_pooler()
+        bert_classifier_average_pooler_model.compile(
+            optimizer=optimizer,
+            loss=[
+                tf.keras.losses.BinaryCrossentropy(from_logits=False),
+            ],
+            metrics=[
+                tf.metrics.BinaryAccuracy(),
+            ],
+        )
+
+        print(bert_classifier_average_pooler_model.summary())
+
+        # exit()
+        history = bert_classifier_average_pooler_model.fit(
+            [train_input_ids, train_token_type_ids, train_attention_mask],
+            [train_labels],
+            shuffle=True,
+            batch_size=batch_size,
+            epochs=epochs,
+            validation_data=(
+                [val_input_ids, val_token_type_ids, val_attention_mask],
+                [val_labels],
+            ),
+            callbacks=[
+                tf.keras.callbacks.ModelCheckpoint(
+                    filepath=checkpoint_fullpath,
+                    verbose=1,
+                    save_weights_only=True,
+                    save_freq="epoch",
+                ),
+            ],
+        )
+
+        print("Save history...")
+        joblib.dump(
+            history.history,
+            f"bert-large-uncased-classifier-average-pooler-model-train-history-{percent_data}.pkl",
+            compress=False,
+            protocol=pickle.HIGHEST_PROTOCOL,
+        )
